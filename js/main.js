61d0fed49b669d637024c28cc32c4292c3a4b9e8
Krasnoe <psp.tema@mail.ru>
2021-03-09 13:13:49 +0300

    Create: added Keyboard Control Methods

--- js/main.js
+++ js/main.js
@@ -7,4 +7,16 @@ const swiper = new Swiper('.swiper-container', {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+});
+
+
+document.addEventListener('keyup', function(event){
+    console.log('Key: 37', event.key);
+    console.log('keyCode: ', event.keyCode);
 });
