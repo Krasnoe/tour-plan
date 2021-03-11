9cdc7122e926d76458366c8a1fa29d102ad28d90
Krasnoe <psp.tema@mail.ru>
2021-03-11 10:57:17 +0300

    fix: fixed block booking

--- js/main.js
+++ js/main.js
@@ -20,3 +20,12 @@ document.addEventListener('keyup', function(event){
     console.log('Key: 37', event.key);
     console.log('keyCode: ', event.keyCode);
 });
+
+type="text/javascript">
+  ymaps.ready(init);
+  function init() {
+    var myMap = new ymaps.Map("map", {
+      center: [7.573638, 79.803867],
+      zoom: 7
+  });
+}
