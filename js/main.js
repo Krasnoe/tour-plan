e6eb2762c7340316b380c755912fca19da87457f
Krasnoe <psp.tema@mail.ru>
2021-03-13 20:04:32 +0300

    fix: fixed all media to lesson-22

--- js/main.js
+++ js/main.js
@@ -32,9 +32,4 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     nextEl: '.reviews-slider__button--next',
     prevEl: '.reviews-slider__button--prev',
   },
-  
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
 });
