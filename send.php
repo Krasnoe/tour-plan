848e95e015f11c1176a0e469322806ea5d330dea
Krasnoe <psp.tema@mail.ru>
2021-03-25 14:43:11 +0300

    fixed any errors in Send.php

--- send.php
+++ send.php
@@ -71,6 +71,5 @@ else {$result = "error";}
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
-// header('Location: thankyou.html');
+header('Location: thankyou.html');
 ?>
